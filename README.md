bf96b1e91fd24aee0ef321519bd227f943025adf
seongho19 <76552350+seongho19@users.noreply.github.com>
2021-07-06 16:37:30 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# projects
+This is not the original one and only just a remake. 
+
---
+++ database.js
@@ -0,0 +1,11 @@
+
+function Translate(input)
+
+phrase = String(input)
+
+if (phrase=="Sawubona") { 
+    output = "Hello (individual)";
+}
+else if (phrase=="Sanibonani") { 
+    output = "Hello (group)";
+}
